c4298f334bff2217f5f53d933cbc60cc20b1c857
Landon Turner <lturner@LTurner-MBP.lan1>
2020-04-15 15:29:46 -0400

    schema skeleton

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,29 @@
 const db = require('../data/db-config');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find(){
+    return
+}
+function findById(){
+    return
+}
+function findSteps(){
+    return
+}
+function add(){
+    return
+}
+function update(){
+    return
+}
+function remove(){
+    return
+}
